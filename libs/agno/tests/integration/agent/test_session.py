327e7760850222a382784880ac1b80ac22f0146c
Dirk Brand <dirkbrnd@gmail.com>
2025-08-07 09:42:11 +0200

    Merge

---
+++ libs/agno/tests/integration/agent/test_session.py
@@ -0,0 +1,128 @@
+import pytest
+from typing import Any, Dict, Optional
+import uuid
+
+from agno.agent.agent import Agent
+from agno.db.sqlite.sqlite import SessionType
+from agno.models.openai.chat import OpenAIChat
+
+
+def chat_agent_factory(shared_db, session_id: Optional[str] = None, session_state: Optional[Dict[str, Any]] = None):
+    """Create an agent with storage and memory for testing."""
+    return Agent(
+        model=OpenAIChat(id="gpt-4o-mini"),
+        db=shared_db,
+        session_id=session_id or str(uuid.uuid4()),
+        session_state=session_state or {},
+    )
+
+
+def test_agent_default_state(shared_db):
+    session_id = "session_1"
+    session_state = {"test_key": "test_value"}
+    chat_agent = chat_agent_factory(shared_db, session_id, session_state)
+
+    response = chat_agent.run("Hello, how are you?")
+    
+    assert response.run_id is not None
+    
+    assert chat_agent.session_id == session_id
+    assert chat_agent.session_state == session_state
+    
+    session_from_storage = shared_db.get_session(session_id=session_id, session_type=SessionType.AGENT)
+    assert session_from_storage is not None
+    assert session_from_storage.session_id == session_id
+    assert session_from_storage.session_data["session_state"] == {
+        "test_key": "test_value",
+    }
+
+def test_agent_set_session_name(shared_db):
+    session_id = "session_1"
+    chat_agent = chat_agent_factory(shared_db, session_id)
+
+    response = chat_agent.run("Hello, how are you?")
+    
+    chat_agent.set_session_name(session_id=session_id, session_name="my_test_session")
+    
+    session_from_storage = shared_db.get_session(session_id=session_id, session_type=SessionType.AGENT)
+    assert session_from_storage is not None
+    assert session_from_storage.session_id == session_id
+    assert session_from_storage.session_data["session_name"] == "my_test_session"
+
+def test_agent_session_state_switch_session_id(shared_db):
+    session_id_1 = "session_1"
+    session_id_2 = "session_2"
+    
+    chat_agent = chat_agent_factory(shared_db, session_id_1, session_state={"test_key": "test_value"})
+
+    # First run with a session ID (reset should not happen)
+    response = chat_agent.run("What can you do?")
+    session_from_storage = shared_db.get_session(session_id=session_id_1, session_type=SessionType.AGENT)
+    assert session_from_storage is not None
+    assert session_from_storage.session_id == session_id_1
+    assert session_from_storage.session_data["session_state"] == {"test_key": "test_value"}
+
+    # Second run with different session ID, and override session state
+    response = chat_agent.run("What can you do?", session_id=session_id_2, session_state={"test_key": "test_value_2"})
+    session_from_storage = shared_db.get_session(session_id=session_id_2, session_type=SessionType.AGENT)
+    assert session_from_storage is not None
+    assert session_from_storage.session_id == session_id_2
+    assert session_from_storage.session_data["session_state"] == {"test_key": "test_value_2"}
+
+    # Third run with the original session ID
+    response = chat_agent.run("What can you do?", session_id=session_id_1)
+    session_from_storage = shared_db.get_session(session_id=session_id_1, session_type=SessionType.AGENT)
+    assert session_from_storage is not None
+    assert session_from_storage.session_id == session_id_1
+    assert session_from_storage.session_data["session_state"] == {"test_key": "test_value"}
+
+
+def test_agent_with_state_on_agent():
+    # Define a tool that increments our counter and returns the new value
+    def add_item(session_state: Dict[str, Any], item: str) -> str:
+        """Add an item to the shopping list."""
+        session_state["shopping_list"].append(item)
+        return f"The shopping list is now {session_state['shopping_list']}"
+
+    # Create an Agent that maintains state
+    agent = Agent(
+        session_state={"shopping_list": []},
+        tools=[add_item],
+        instructions="Current state (shopping list) is: {shopping_list}",
+        # Add the state to the messages
+        add_state_in_messages=True,
+        markdown=True,
+    )
+    agent.run("Add oranges to my shopping list")
+    response = agent.run(
+        'Current shopping list: {shopping_list}. Other random json ```json { "properties": { "title": { "title": "a" } } }```'
+    )
+    assert (
+        response.messages[1].content
+        == 'Current shopping list: [\'oranges\']. Other random json ```json { "properties": { "title": { "title": "a" } } }```'
+    )
+
+
+def test_agent_with_state_on_run():
+    # Define a tool that increments our counter and returns the new value
+    def add_item(session_state: Dict[str, Any], item: str) -> str:
+        """Add an item to the shopping list."""
+        session_state["shopping_list"].append(item)
+        return f"The shopping list is now {session_state['shopping_list']}"
+
+    # Create an Agent that maintains state
+    agent = Agent(
+        tools=[add_item],
+        instructions="Current state (shopping list) is: {shopping_list}",
+        # Add the state to the messages
+        add_state_in_messages=True,
+        markdown=True,
+    )
+    agent.run("Add oranges to my shopping list", session_id="session_1", session_state={"shopping_list": []})
+    response = agent.run(
+        'Current shopping list: {shopping_list}. Other random json ```json { "properties": { "title": { "title": "a" } } }```', session_id="session_1"
+    )
+    assert (
+        response.messages[1].content
+        == 'Current shopping list: [\'oranges\']. Other random json ```json { "properties": { "title": { "title": "a" } } }```'
+    )
